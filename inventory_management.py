175a7da34ff64bf09c6bf2f95f841bdb628a91a4
itsambrous <125042565+itsambrous@users.noreply.github.com>
2023-02-10 15:25:46 +0100

    Update inventory_management.py

--- inventory_management.py
+++ inventory_management.py
@@ -1,33 +1,35 @@
 # Creating Inventory 
 
 class Inventory:
-    def __init__(self, item_name, quantity):
+    def __init__(self, item_name, quantity, price):
         self.item_name = item_name
         self.quantity = quantity
+        self.price = price
 
 inventory = []
 
-def add_item(item_name, quantity):
-    new_item = Inventory(item_name, quantity)
+def add_item(item_name, quantity, price):
+    new_item = Inventory(item_name, quantity, price)
     inventory.append(new_item)
 
-def update_item(item_name, quantity):
+def update_item(item_name, quantity, price):
     for item in inventory:
         if item.item_name == item_name:
             item.quantity = quantity
+            item.price = price
 
 def report():
     for item in inventory:
-        print("Item: {}\tQuantity: {}".format(item.item_name, item.quantity))
+        print("Item: {}\tQuantity: {}\tPrice: {:.2f} €".format(item.item_name, item.quantity, item.price))
 
 # Example usage
-add_item("apple", 10)
-add_item("banana", 5)
+add_item("oranges", 10, 0.5)
+add_item("banana", 5, 0.3)
 
 print("Original inventory:")
 report()
 
-update_item("banana", 10)
+update_item("banana", 10, 0.4)
 
 print("\nUpdated inventory:")
 report()
