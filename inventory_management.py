d07e8423363f5d140095cdc1ac661f910c941a98
itsambrous <125042565+itsambrous@users.noreply.github.com>
2023-02-14 15:59:11 +0100

    Update inventory_management.py

--- inventory_management.py
+++ inventory_management.py
@@ -23,13 +23,13 @@ def report():
         print("Item: {}\tQuantity: {}\tPrice: {:.2f} €".format(item.item_name, item.quantity, item.price))
 
 # Example usage
-add_item("oranges", 10, 0.5)
-add_item("banana", 5, 0.3)
+add_item("Silk Ties", 80, 95)
+add_item("Bow-Ties", 100, 60)
 
 print("Original inventory:")
 report()
 
-update_item("banana", 10, 0.4)
+update_item("Silk Ties", 75, 90)
 
 print("\nUpdated inventory:")
 report()
